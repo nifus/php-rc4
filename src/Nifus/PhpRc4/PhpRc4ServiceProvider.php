715896f4cb802686312941f002c4640d1fb9a054
nifus <nifus@mail.ru>
2014-11-13 17:46:26 +0300

    format composer

---
+++ src/Nifus/PhpRc4/PhpRc4ServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Nifus\PhpRc4;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhpRc4ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+    }
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+    public function boot()
+    {
+        
+    }
+
+}
